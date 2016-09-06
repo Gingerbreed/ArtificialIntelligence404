5fb490b9502a9fe5474c2cd9b9c9f45dd0561524
Morgan Baker <morgan.baker2@marist.edu>
2016-09-06 04:29:16 -0400

    Homework 0 Complete!
    Just finished Homework 0! The whole LaTeX thing is a little confusing,
    but it's done!

---
+++ baker-00/Confirmation.py
@@ -0,0 +1,13 @@
+import sys
+import scipy
+import numpy
+import sklearn
+import matplotlib
+import pandas
+
+print(sys.__version__)
+print(matplotlib.__version__)
+print(numpy.__version__)
+print(scipy.__version__)
+print(sklearn.__version__)
+print(pandas.__version__)
---
+++ baker-00/Matrix.py
@@ -0,0 +1,4 @@
+import numpy
+x = numpy.matrix(((1,2), (4,-1), (-3,3)))
+y = numpy.matrix(((2,0,5), (0,-1,4)))
+x*y
